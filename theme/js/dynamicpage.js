74c93b2c69e7e7f3268a535b706c3bf3956c9dcd
Alex Pearson <pearsonn.alex@gmail.com>
2015-06-14 16:15:03 +0000

    Fresh start 061415

--- theme/js/dynamicpage.js
+++ theme/js/dynamicpage.js
@@ -1,41 +1,53 @@
 $(function() {
 
-    var newHash      = "",
-        $mainContent = $("#content"),
-        $pageWrap    = $("#content-wrapper"),
-        baseHeight   = 0,
-        $el;
-        
-    $pageWrap.height($pageWrap.height());
-    baseHeight = $pageWrap.height() - $mainContent.height();
-    
     $("nav").delegate("a", "click", function() {
-        window.location.hash = $(this).attr("href");
-        return false;
+
+      _href = $(this).attr("href");
+
+      // change the url without a page refresh and add a history entry.
+      history.pushState(null, null, _href);
+
+      // load the content
+      loadContent(_href); // fear not! we're going to build this function in the next code block
+
     });
     
-    $(window).bind('hashchange', function(){
-    
-        newHash = window.location.hash.substring(1);
-        
-        if (newHash) {
-            $mainContent
-                .find("#guts")
-                .fadeOut(200, function() {
-                    $mainContent.hide().load(newHash + " #guts", function() {
-                        $mainContent.fadeIn(200, function() {
-                            $pageWrap.animate({
-                                height: baseHeight + $mainContent.height() + "px"
-                            });
-                        });
-                        $("nav a").removeClass("current");
-                        $("nav a[href='"+newHash+"']").addClass("current");
-                    });
-                });
-        };
-        
+    var $mainContent = $("#content-wrapper"),
+    $pageWrap    = $("#page-wrap"),
+    baseHeight   = 0,
+    $el;
+
+// calculate wrapper heights to prevent jumping when loading new content
+$pageWrap.height($pageWrap.height());
+baseHeight = $pageWrap.height() - $mainContent.height();
+
+function loadContent(href) {
+
+  $mainContent
+    .find("#content")
+    .fadeOut(200, function() { // fade out the content of the current page
+      $mainContent
+        .hide()
+        .load(href + " #content", function() { // load the contents of whatever href is
+          $mainContent.fadeIn(200, function() {
+            $pageWrap.animate({
+              height: baseHeight + $mainContent.height() + &quot;px&quot;
+            });
+         });
+      
+      $("nav a").removeClass("current");
+
+      $("nav a[href$='" + href + "']").addClass("current");
+
     });
-    
-    $(window).trigger('hashchange');
+
+  });
+
+}
+
+$(window).bind("popstate", function() {
+    link = location.pathname.replace(/^.*[\\/]/, ""); // get filename only
+    loadContent(link);
+});
 
 });
