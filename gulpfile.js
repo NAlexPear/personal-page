218fe77951ea2a4aec7d7c10c43a1821dab37416
Alex Pearson <alex@alexpear.com>
2015-10-26 17:41:30 -0500

    Continue building gulpfile.js, deploying to public directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
+var header = require('gulp-header');
 
-gulp.task('default', function() {
+gulp.task('default', ['add-headers', 'bower-port'], function() {
    gulp.src('index.html')
         .pipe(usemin({
             assetsDir: 'public',
@@ -11,4 +12,23 @@ gulp.task('default', function() {
             js: [uglify(), 'concat']
         }))
         .pipe(gulp.dest('public'));
+});
+
+gulp.task('add-headers', function() {
+    gulp.src('index.html')
+        .pipe(header("<!-- This file is generated — do not edit by hand! -->\n"))
+        .pipe(gulp.dest('public'));
+
+    gulp.src('public/theme/js/site.js')
+        .pipe(header("/* This file is generated — do not edit by hand! */\n"))
+        .pipe(gulp.dest('public/theme/js'));
+
+    gulp.src('public/theme/css/site.css')
+        .pipe(header("/* This file is generated — do not edit by hand! */\n"))
+        .pipe(gulp.dest('public/theme/css'));
+});
+
+gulp.task('bower-port', function(){
+  gulp.src(['bower_components/**/*'])
+    .pipe(gulp.dest('public/bower_components'));
 });
