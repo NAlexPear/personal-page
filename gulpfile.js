757904d78344780d3b4b3ed96ad6565dca4a9571
Alex Pearson <alex@alexpear.com>
2015-10-29 14:31:32 -0500

    Fully implement gulp process for jekyll blog

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var header = require('gulp-header');
 var imagemin = require('gulp-imagemin');
-var exex = require('child_process').exec;
+var exec = require('child_process').exec;
 var gutil = require('gulp-util');
 
 
 
-gulp.task('default', ['add-headers', 'bower-port', 'misc-port','image-min', 'image-port'], function() {
+gulp.task('default', ['add-headers', 'bower-port', 'misc-port','image-min', 'image-port', 'jekyll'], function() {
    gulp.src('index.html')
         .pipe(usemin({
             assetsDir: '',
@@ -52,7 +52,7 @@ gulp.task('image-port', function () {
         .pipe(gulp.dest('public/theme/images'));
 });
 gulp.task('jekyll', function (){
-exec('jekyll build --source blog/ --destination public/blog', function(err, stdout, stderr) {
+exec('jekyll build --source blog/ --destination public/blog/', function(err, stdout, stderr) {
       console.log(stdout);
   });
 });
