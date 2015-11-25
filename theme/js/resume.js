71b1d383c485f43ef4ebb8d72d03721ff74e71fc
Alex Pearson <alex@alexpear.com>
2015-11-25 15:59:13 -0600

    Re-implement resume dropdown with slideUp and slideDown

--- theme/js/resume.js
+++ theme/js/resume.js
@@ -1,13 +1,13 @@
 $(function () {
   $('.resume-drop>ul').on('click','li',function(){
-    if($(this).children('div').hasClass('hidden')){
-      $(this).siblings().children('div').addClass('hidden');
-      $(this).children('div').removeClass('hidden');
+    if($(this).children('div').hasClass('expanded')){
+      $(this).children('div').slideUp('fast').removeClass('expanded');
+      $(this).removeClass('active');
+    } else {
+      $(this).siblings().children('div').removeClass('expanded').slideUp('fast');
+      $(this).children('div').slideDown('fast').addClass('expanded');
       $(this).addClass('active');
       $(this).siblings().removeClass('active');
-    } else {
-      $(this).children('div').addClass('hidden');
-      $(this).removeClass('active');
     }
   });
 });
